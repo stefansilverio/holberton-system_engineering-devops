9e0662a7ec07874862beaa9819d05216fa0e2864
stefansilverio <494@holbertonschool.com>
2019-05-06 21:19:21 +0000

    gather data re a specific employee

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""return TODO list progress"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    users = requests.get("https://jsonplaceholder.typicode.com/users/" + sys.argv[1])
+    user = users.json().get("name")
+    all_tasks = requests.get("https://jsonplaceholder.typicode.com/todos?userId=" + sys.argv[1])
+    all_tasks_list = all_tasks.json()
+    total_tasks = len(all_tasks_list)
+    done_tasks = 0
+    done_task_titles = []
+    for task in all_tasks_list:
+        if task.get("completed") == True:
+            done_tasks += 1
+            done_task_titles.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(user, done_tasks, total_tasks))
+    for title in done_task_titles:
+        print(title)
