7794bd9409242e39ac006ff8f7be394063d43dc2
stefansilverio <494@holbertonschool.com>
2019-05-09 22:16:03 +0000

    print titles of first 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""print titles of hot top ten"""
+import sys
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """print hot top ten"""
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+    url = 'https://www.reddit.com/api/search_reddit_names.json'
+    valid = requests.get(url, headers=headers, params={'query': subreddit})\
+                    .json()
+    if subreddit in valid['names']:
+        top = requests.get('https://www.reddit.com/r/' + subreddit + '.json',
+                           headers=headers).json()
+        cnt = 0
+        for dicti in top['data']['children']:
+            if cnt < 10:
+                print(dicti['data']['title'])
+            cnt += 1
