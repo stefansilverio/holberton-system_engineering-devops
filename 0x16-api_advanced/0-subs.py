e219244471508c92e727a5a788a04fe4658d5fdc
stefansilverio <494@holbertonschool.com>
2019-05-09 22:28:36 +0000

    return number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,17 @@ import sys
 
 def number_of_subscribers(subreddit):
     """find number of subscribers"""
-    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     headers = {
         'User-Agent': 'Mozilla/5.0'
     }
-    cnt = requests.get(url, headers=headers)
-    try:
-        (cnt.json()['data']['subscribers'])
-    except KeyError:
+    url = 'https://www.reddit.com/api/search_reddit_names.json'
+    valid = requests.get(url, headers=headers, params={'query': subreddit})\
+                    .json()
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+
+    if subreddit in valid['names']:
+        sub = requests.get(url,
+                           headers=headers).json()
+        return (sub['data']['subscribers'])
+    else:
         return (0)
-    return (cnt.json()['data']['subscribers'])
