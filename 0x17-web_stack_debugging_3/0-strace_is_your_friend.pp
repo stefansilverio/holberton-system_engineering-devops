047a06bb5a6383727c8ed0691f98e3a473ade9c4
stefansilverio <stefansilverio@gmail.com>
2019-05-23 00:03:27 +0000

    fix newline in puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # file fixes bad php extension in /var/www/html/wp-settings.php
 exec { '/var/www/html/wp-settings.php':
-          command => '/bin/sed -i "137s/phpp/php/"
-	   /var/www/html/wp-settings.php',
+          command => '/bin/sed -i "137s/phpp/php/" \
+	  /var/www/html/wp-settings.php',
           path    => '/var/www/html/wp-settings.php'
 }
