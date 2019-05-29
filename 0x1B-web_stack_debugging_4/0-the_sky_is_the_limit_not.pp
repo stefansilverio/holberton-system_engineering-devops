0bf747bfb6f5570f19c0ed7bd94c464d21b06800
stefansilverio <stefansilverio@gmail.com>
2019-05-29 19:50:58 +0000

    change nginx connection limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# this puppet manifest fixes too many open files error
+exec { 'update ulimit for nginx':
+  command => '/bin/echo ULIMIT="-n 5000" > /etc/default/nginx',
+  path    => '/etc/default/nginx'
+}
+
+exec { 'restart server':
+  command => '/usr/bin/sudo service nginx restart'
+}
