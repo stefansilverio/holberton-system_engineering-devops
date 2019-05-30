101cb45a876668a1378ed9c6bcbcfa83d7d1880b
stefansilverio <stefansilverio@gmail.com>
2019-05-30 16:46:47 +0000

    fix quotation error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # this puppet manifest fixes too many open files error
 exec { 'update ulimit for nginx':
-  command => '/bin/echo ULIMIT="-n 5000" > /etc/default/nginx',
+  command => "/bin/echo ULIMIT='-n 5000' > /etc/default/nginx",
   path    => '/etc/default/nginx'
 }
 
